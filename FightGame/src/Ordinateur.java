a52799abff686067ac5850089a50f993686ed2dd
AntiDotStudio <antidotstudio@gmail.com>
2019-10-12 18:00:50 -0400

    Game Mecanique

--- FightGame/src/Ordinateur.java
+++ FightGame/src/Ordinateur.java
@@ -1,17 +1,20 @@
+/*
+ * Classe "Ordinateur" héritant de "Joueur"
+ */
 
 public class Ordinateur extends Joueur 
 {
+	/*
+	 * Constructeur de Joueur
+	 */
+	
 	public Ordinateur(int[] nbSous)
 	{
 		super(nbSous);
 	}
 	
-	public Ordinateur(int nbSous1, int nbSous5, int nbSous10, int nbSous25)
-	{
-		super(nbSous1, nbSous5, nbSous10, nbSous25);
-	}
 	
-	public void IA()
+	public void IA(int[] pot)
 	{
 		// A implémenter
 	}
@@ -22,7 +25,7 @@ public class Ordinateur extends Joueur
 	public String toString(int[] valeurs) 
 	{
 		return "ORDINATEUR\n"
-				+ (sous != null ? arrayToString(sous, sous.length, valeurs) : null)
+				+ (this.sous != null ? arrayToString(this.sous, this.sous.length, valeurs) : null)
 				+ "";
 	}
 }
